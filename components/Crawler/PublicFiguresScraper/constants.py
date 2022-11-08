14c7d2f40507b738953626ab68add10d765fad93
AdrianSmau <smauadrian@gmail.com>
2022-11-09 00:18:54 +0200

    finished implementing CSV scraper

--- components/Crawler/PublicFiguresScraper/constants.py
+++ components/Crawler/PublicFiguresScraper/constants.py
@@ -13,9 +13,15 @@ TIP_DECLARATIE_INPUT_ID = r'form:Tip_input'
 STERGE_FILTRE_BUTON_ID = r'form:clearButton'
 CAUTA_BUTON_ID = r'form:submitButtonAS'
 BODY_TABEL_ID = r'form:resultsTable_body'
+SEARCH_RESULT_ID = r'form:j_idt80_content'
 RAND_TABEL_ID_PREFIX = r'form:resultsTable_row_'
+TOO_MANY_RECORDS = r'errorForm'
+TOO_MANY_RECORDS_BUTTON = r'errorForm:inchide'
 DESCARCA_DOCUMENT_BUTON_CLASA = r"null ui-col-7 ui-datatable-first"
 DESCARCA_DOCUMENT_BUTON_TEXT = "Vezi document"
+
+DESCARCA_CSV = r'form:dataExporter'
+PAGINATION_BAR = r'form:resultsTable_paginatorbottom'
 
 # CONSTANTE APLICATIE
 LOCATIE_DESCARCARE = "./data/declarations"
--- components/Crawler/PublicFiguresScraper/scraper.py
+++ components/Crawler/PublicFiguresScraper/scraper.py
@@ -3,7 +3,7 @@ import os.path
 import time
 
 from selenium import webdriver
-from selenium.common import TimeoutException
+from selenium.common import TimeoutException, NoSuchElementException
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.select import Select
 from selenium.webdriver.support.wait import WebDriverWait
@@ -23,62 +23,70 @@ def get_selenium_driver():
 def search_declaratii_integritate(driver):
     buton_cauta_elem = driver.find_element("id", CAUTA_BUTON_ID)
     buton_cauta_elem.click()
+    print("Performing new search!...")
+    time.sleep(4)
 
 
-def descarca_declaratii_integritate(driver, count):
+def descarca_declaratii_integritate(driver):
     try:
-        element_present = EC.presence_of_element_located(("id", BODY_TABEL_ID))
-        WebDriverWait(driver, 5).until(element_present)
+        element_present = EC.presence_of_element_located(("id", SEARCH_RESULT_ID))
+        WebDriverWait(driver, 15).until(element_present)
     except TimeoutException:
-        print("Timed out waiting for page to load")
+        print("Timed out waiting for results to load")
 
-    # if not count:
-    #     pass  # TODO -> implement next page accessing and download all declarations
-    # else:
-    #     body_tabel_elem = driver.find_element("id", BODY_TABEL_ID)
-    #     for row in body_tabel_elem.find_elements("css selector", "*")[
-    #                ::2]:  # Mergem din 2 in 2 ca imi dubleaza elementele for some reason
-    #         print(row.text)
-    #         if row.text == DESCARCA_DOCUMENT_BUTON_TEXT:
-    #             row.click()
+    print("Searching for results!...")
+    time.sleep(1)
+
+    if driver.find_element("id", TOO_MANY_RECORDS).is_displayed():
+        print("Too many records returned!...")
+        time.sleep(4)
+        driver.find_element("id", TOO_MANY_RECORDS_BUTTON).click()
+    else:
+        try:
+            download_button = driver.find_element("id", DESCARCA_CSV)
+            download_button.click()
+            print("Downloading CSV!...")
+            time.sleep(4)
+        except NoSuchElementException:
+            print("No records found!...")
+            time.sleep(4)
 
 
 def scrape_declaratii_integritate(driver):
     open_advanced_search_page(driver)
 
-    start_date = datetime.date(year=2020, month=10, day=30)
-    end_date = datetime.date(year=2022, month=10, day=30)
+    start_date = datetime.date(year=2020, month=11, day=1)
+    end_date = datetime.date(year=2022, month=11, day=1)
 
     current_start_date = start_date
     while current_start_date <= end_date:
-        current_end_date = current_start_date + datetime.timedelta(days=2)
-
-        tip_declaratie_elem = driver.find_element("id", TIP_DECLARATIE_INPUT_ID)
-        select_declaratie = Select(tip_declaratie_elem)
-        select_declaratie.select_by_visible_text("Declaratie de avere")
-
-        data_inceput_elem = driver.find_element("id", DATA_INCEPUT_INPUT_ID)
-        data_inceput_elem.send_keys(current_start_date.strftime('%d.%m.%Y'))
-
-        data_sfarsit_elem = driver.find_element("id", DATA_SFARSIT_INPUT_ID)
-        data_sfarsit_elem.send_keys(current_end_date.strftime('%d.%m.%Y'))
-
-        for localitate_index in range(1, 3167):
+        current_end_date = current_start_date + datetime.timedelta(days=73)
+        for localitate_index in range(3, 3167):
             try:
-                element_present = EC.presence_of_element_located(("id", DATA_INCEPUT_INPUT_ID))
-                WebDriverWait(driver, 3).until(element_present)
+                element_present = EC.presence_of_element_located(("id", LOCALITATE_INPUT_ID))
+                WebDriverWait(driver, 10).until(element_present)
             except TimeoutException:
-                print("Timed out waiting for page to load")
+                print("Timed out waiting for advanced search page to load")
+
+            data_inceput_elem = driver.find_element("id", DATA_INCEPUT_INPUT_ID)
+            data_inceput_elem.clear()
+            data_inceput_elem.send_keys(current_start_date.strftime('%d.%m.%Y'))
+
+            data_sfarsit_elem = driver.find_element("id", DATA_SFARSIT_INPUT_ID)
+            data_sfarsit_elem.clear()
+            data_sfarsit_elem.send_keys(current_end_date.strftime('%d.%m.%Y'))
+
+            tip_declaratie_elem = driver.find_element("id", TIP_DECLARATIE_INPUT_ID)
+            select_declaratie = Select(tip_declaratie_elem)
+            select_declaratie.select_by_visible_text("Declaratie de avere")
 
             locatie_elem = driver.find_element("id", LOCALITATE_INPUT_ID)
             select_locatie = Select(locatie_elem)
             select_locatie.select_by_index(localitate_index)
 
             search_declaratii_integritate(driver)
-            descarca_declaratii_integritate(driver, 1)
+            descarca_declaratii_integritate(driver)
 
-        data_inceput_elem.clear()
-        data_sfarsit_elem.clear()
         current_start_date += datetime.timedelta(days=1)
 
 
@@ -91,7 +99,7 @@ def open_advanced_search_page(driver):
         element_present = EC.presence_of_element_located(("id", NUME_PRENUME_INPUT_ID))
         WebDriverWait(driver, LOADING_TIMEOUT).until(element_present)
     except TimeoutException:
-        print("Timed out waiting for page to load")
+        print("Timed out waiting for advanced search page to load")
 
 
 def scrape(args):
